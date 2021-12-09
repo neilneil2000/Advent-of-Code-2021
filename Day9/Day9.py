a3cdd372c058f6212c1c0c4781cda3bcaa4962d2
neilneil2000 <31366098+neilneil2000@users.noreply.github.com>
2021-12-09 18:19:45 +0000

    Final Submission Day 9

--- Day9/Day9.py
+++ Day9/Day9.py
@@ -3,9 +3,20 @@ from map import Map
 def main():
     cave = Map("Day9/DayNineInput")
     cave.find_low_points()
-    print(f'Found {len(cave.low_points)} Low Points')
     risk = cave.calculate_risk()
+    print(f'DAY 9 PART 1 COMPLETE:')
+    print(f'======================')
     print(f'Risk Level is {risk}')
+    print()
+    cave.mark_basins()
+    cave.calculate_basin_sizes()
+    cave.sort_basin_sizes()
+    answer = cave.basin_sizes.pop() * cave.basin_sizes.pop() * cave.basin_sizes.pop()
+    print(f'DAY 9 PART 2 COMPLETE:')
+    print(f'======================')
+    print(f'Product of LARGEST 3 basins is: {answer}')
+    print()
+
 
 
 if __name__ == '__main__':

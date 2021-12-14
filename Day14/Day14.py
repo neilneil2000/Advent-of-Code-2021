b713cb2c388cd0b9ea87704874ed3b3074ce6f3d
Neil <31366098+neilneil2000@users.noreply.github.com>
2021-12-14 12:39:21 +0000

    Day 14 Part 2 Complete

--- Day14/Day14.py
+++ Day14/Day14.py
@@ -2,12 +2,11 @@ from polymer import Polymer
 
 def main():
     my_polymer = Polymer("Day14\DayFourteenInput")
-    print(my_polymer.polymer_chain)
-    my_polymer.run_insertion_steps(10)
+    my_polymer.run_insertion_steps(40)
     my_polymer.count_monomers()
     my_polymer.calculate_min_max()
-    print(f'Highest = {my_polymer.highest}')
-    print(f'Lowest = {my_polymer.lowest}')
+    print(f'Highest = {my_polymer.highest}: {my_polymer.monomer_count[my_polymer.highest]}')
+    print(f'Lowest = {my_polymer.lowest}: {my_polymer.monomer_count[my_polymer.lowest]}')
     print(f'Answer is: {my_polymer.monomer_count[my_polymer.highest] - my_polymer.monomer_count[my_polymer.lowest]}')
     print("END")
 
--- Day14/polymer.py
+++ Day14/polymer.py
@@ -2,10 +2,10 @@ from typing import List
 
 
 class Polymer:
-
     def __init__(self,filename):
-        self.polymer_chain = []
         self.monomer_count = {}
+        self.pair_count = {}
+        self.end_monomer = set()
         self.rules = {}
         self.read_input_file(filename)
 
@@ -15,22 +15,33 @@ class Polymer:
         self.lowest = min(self.monomer_count,key=self.monomer_count.get)
 
     def count_monomers(self):
-        unique_monomers = set(self.polymer_chain)
-        for monomer in unique_monomers:
-            count = self.polymer_chain.count(monomer)
-            self.monomer_count.update({monomer:count})
+        monomers = {}
+        for pair in self.rules.keys():
+            monomers.update({pair[0]:0})
+            monomers.update({pair[1]:0})
+        for pair in self.pair_count:
+            for monomer in pair:
+                count = monomers[monomer] + self.pair_count[pair]
+                monomers.update({monomer:count})
+        for monomer in monomers:
+            if monomer in self.end_monomer:
+                monomers[monomer] +=1
+            monomers[monomer] = int(monomers[monomer]/2)
+        self.monomer_count = monomers
 
     def run_insertion_steps(self,number_of_steps):
         for _ in range(number_of_steps):
             self.run_insertion_step()
 
     def run_insertion_step(self):
-        new_monomers = []
-        for index in range(1,len(self.polymer_chain)):
-            pair = self.polymer_chain[index-1],self.polymer_chain[index]
-            new_monomers.append(self.rules[pair])
-        for index,monomer in enumerate(new_monomers):
-            self.polymer_chain.insert(2*index+1,monomer)
+        new_pair_count = self.blank_pair_count.copy()
+        for pair in self.pair_count:
+            rules = self.rules[pair]
+            for rule in rules:
+                count = new_pair_count[rule]
+                existing_count = self.pair_count[pair]
+                new_pair_count.update({rule:count+existing_count})
+        self.pair_count = new_pair_count
 
 
     def read_input_file(self,filename):
@@ -44,9 +55,23 @@ class Polymer:
         for line in input:
             if line.find('->') >= 0:
                 line = line.split('->')
-                self.rules.update({(line[0][0].strip(),line[0][1].strip()):line[1].strip()})
+                input = list(line[0].strip())
+                input.insert(1,line[1].strip())
+                new_rule = {
+                    (input[0],input[2]):[(input[0],input[1]),(input[1],input[2])]
+                    }
+                self.rules.update(new_rule)
             elif line == '':
                 pass
             else:
-                self.polymer_chain.extend(list(line))
-
+                polymer_chain = line
+        #Set Starting Position
+        self.end_monomer.add(polymer_chain[0])
+        self.end_monomer.add(polymer_chain[-1])
+        for key in self.rules.keys():
+            self.pair_count.update({key:0})
+        self.blank_pair_count = self.pair_count.copy()
+        for x in range(1,len(polymer_chain)):
+            pair = (polymer_chain[x-1],polymer_chain[x])
+            count = self.pair_count[pair]
+            self.pair_count.update({pair:count+1})

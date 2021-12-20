e2ffc4171e0a7084ccd364c8929973aa98ef27bc
Neil <31366098+neilneil2000@users.noreply.github.com>
2021-12-20 18:05:08 +0000

    Day 16 part 1

--- Day16/Day16.py
+++ Day16/Day16.py
@@ -1,18 +1,26 @@
 from Packet import Packet
 
 def main():
-    input = read_input_file("Day16\DaySixteenTestInput")
+    input = read_input_file("Day16\DaySixteenInput")
     print(input)
-    binary_packet = bin(int(input[0],16))
-    binary_packet = binary_packet[2:]
-    outer_packet = Packet(binary_packet)
+    binary_string = bin(int(input[0],16))
+    binary_string = binary_string[2:] #Remove '0b' from front
+    preceeding_zeroes = 4 - (len(binary_string) % 4)
+    if preceeding_zeroes == 4:
+        preceeding_zeroes = 0
+    for _ in range(0, preceeding_zeroes):
+        binary_string = '0' + binary_string
+    outer_packet = Packet(binary_string)
+    total_version = outer_packet.get_total_version()
+    print(f'Sum of Version Numbers: {total_version}')
+    print(f'END')
 
 
 def read_input_file(filename):
     input = []
     with open(filename,'r') as f:
         for line in f:
-            input.append(line)
+            input.append(line.strip())
     return input
 
 if __name__ == '__main__':
--- Day16/Packet.py
+++ Day16/Packet.py
@@ -1,3 +1,6 @@
+from os import get_inheritable
+
+
 class Packet:
     """
     <----- HEADER ----->
@@ -9,11 +12,11 @@ class Packet:
     
                           LITERAL  PACKET 
     <----- HEADER -----><------------------ BODY ------------------->
-    ----------------------------------- ~ ~ ---------------   -----
+    ----------------------------------- ~ ~ --------------- ~ -----
     | VERSION |   TYPE  | N |  Value  |     |   |  Value  |   | 0 |
-    ----------------------------------- ~ ~ --------------- ~ ----- ~> n 0s to make total body lengt divisible by 4
+    ----------------------------------- ~ ~ --------------- ~ ----- ~> n 0s to make total body length divisible by 4
     |  X X X  |  1 0 0  | 1 | X X X X |     | 0 | X X X X |   | 0 |
-    ----------------------------------- ~ ~ ---------------   -----
+    ----------------------------------- ~ ~ --------------- ~ -----
 
                           OPERATOR  PACKET 
     <----- HEADER -----><------------------ BODY ------------------->
@@ -30,44 +33,71 @@ class Packet:
     LITERAL_TYPE = 4
     OP_MODE_BITS = '0'
     OP_MODE_PACKETS = '1'
-    OP_MODE_BITS_BITS = 15
-    OP_MODE_PACKETS_BITS = 11
+    OP_MODE_BITS_LENGTH = 15
+    OP_MODE_PACKETS_LENGTH = 11
 
-    KEEP_READING = 1
-    LAST_DIGIT = 0
+    KEEP_READING = '1'
+    LAST_DIGIT = '0'
     LITERAL_DIGIT_LENGTH = 4
 
     def __init__(self,binary_string):
+    
         self.header_string = binary_string[:Packet.HEADER_LENGTH]
         self.body_string = binary_string[Packet.HEADER_LENGTH:]
         self.version = int(self.header_string[:3],2)
         self.type = int(self.header_string[3:],2)
-        self.pointer = 0
+        self.literal_value = None
+        self.subpackets = []
+        self.length = None
         self.process_packet()
     
+
+
+    def get_total_version(self):
+        total_version = 0
+        for packet in self.subpackets:
+            total_version += packet.get_total_version()
+        total_version += self.version
+        return total_version
+
+    def get_length(self):
+        return len(self.body_string) + len(self.header_string)
+
     def process_literal(self):
         flag = Packet.KEEP_READING
         pointer = 0
-        value_string = []
+        value_string = ''
         while flag == Packet.KEEP_READING:
             flag = self.body_string[ pointer ]
             pointer += 1
             digit = self.body_string[ pointer : pointer + Packet.LITERAL_DIGIT_LENGTH ]
-            value_string.append(int(digit))
-        self.literal = int(value_string)
+            pointer += Packet.LITERAL_DIGIT_LENGTH
+            value_string += digit
+        self.literal = int(value_string,2)
+        self.body_string = self.body_string[:pointer]
 
     def process_operator(self):
-        op_bit = self.body_string[ self.pointer ]
-        self.pointer += 1
-        if op_bit == Packet.OP_MODE_BITS:
-            bits = self.body_string[ self.pointer : Packet.OP_MODE_BITS_BITS + self.pointer ]
-            self.pointer += Packet.OP_MODE_BITS_BITS
-            length_of_packets = int(bits,2)
+        pointer = 0
+        length_type_id = self.body_string[pointer]
+        pointer += 1
+        if length_type_id == Packet.OP_MODE_BITS:
+            bits = self.body_string[ pointer : Packet.OP_MODE_BITS_LENGTH + pointer ]
+            pointer += Packet.OP_MODE_BITS_LENGTH
+            remaining_bits = int(bits,2)
+            while remaining_bits > 0:
+                self.subpackets.append(Packet(self.body_string[pointer:]))
+                length = self.subpackets[-1].get_length()
+                pointer += length
+                remaining_bits -= length         
         else: #op_bit == Packet.OP_MODE_PACKETS
-            bits = self.body_string[ self.pointer : Packet.OP_MODE_PACKETS_BITS + self.pointer ]
-            self.pointer += Packet.OP_MODE_PACKETS_BITS
-            number_of_packets = int(bits,2)
-        pass
+            packets = self.body_string[ pointer : Packet.OP_MODE_PACKETS_LENGTH + pointer ]
+            pointer += Packet.OP_MODE_PACKETS_LENGTH
+            number_of_packets = int(packets,2)
+            for _ in range(0,number_of_packets):
+                self.subpackets.append(Packet(self.body_string[pointer:]))
+                pointer += self.subpackets[-1].get_length()
+        self.body_string = self.body_string[:pointer]
+
 
 
     def process_packet(self):

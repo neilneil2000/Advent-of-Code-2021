7d6ca6b0f3401d2624f8481de8c7a17d0d20f6b3
Neil <31366098+neilneil2000@users.noreply.github.com>
2021-12-21 22:49:45 +0000

    Tidy Up and Simplify

--- Day21/Day21.py
+++ Day21/Day21.py
@@ -3,14 +3,14 @@ from Game import Game
 
 def main():
     starting_places = [10,3]
+
     practice_game = Game(starting_places)
     score, rolls = practice_game.play_game(1000)
     print(f'Winning Score: {score} after {rolls} rolls. Answer = {score*rolls}')
 
     dirac_game = DiracGame(starting_places)
-    dirac_game.play_full_game()
-    print(dirac_game.winning_universes)
-    print(f'END')
+    winning_score = dirac_game.play_full_game()
+    print(f'Winning Player Wins in {winning_score} universes')
 
 if __name__ == "__main__":
     main()
--- Day21/DiracGame.py
+++ Day21/DiracGame.py
@@ -1,114 +1,108 @@
 class DiracGame:
 
-    def __init__(self,starting_positions):
-        self.multiplier = 3 * 3 * 3
-        self.players = 2
-        self.player_grids = [DiracGridManager() for _ in range(self.players)]
-        self.winning_universes = [0,0]
+    def __init__(self,starting_positions) -> None:
+        self.die_values = 3
+        self.die_rolls_per_turn = 3
+        self.multiplier = self.die_values ** self.die_rolls_per_turn
+        self.no_players = 2
+        self.players = [DiracPlayer() for _ in range(self.no_players)]
+        for i in range(self.no_players):
+            self.players[i].set_start_position(starting_positions[i])
         self.active_player = 0
-        p1_grid = self.player_grids[0].grid.grid
-        p2_grid = self.player_grids[1].grid.grid
-        p1_start, p2_start = starting_positions
-        p1_grid[0][p1_start] = 1
-        p2_grid[0][p2_start] = 1
 
+    def determine_winner(self) -> int:
+        winning_score = 0
+        for player in self.players:
+            winning_score = max(winning_score, player.wins)
+        return int(winning_score)
 
-    def play_full_game(self):
-        while self.player_grids[0].active_pawns() > 0 and self.player_grids[1].active_pawns() > 0:
+    def play_full_game(self) -> int:
+        while self.players[0].active_pawns() > 0 and self.players[1].active_pawns() > 0:
             self.take_turn()
+        return self.determine_winner()
 
-    def next_player(self):
+    def next_player(self) -> None:
         player_id = self.active_player
         player_id += 1
         if player_id > 1:
             player_id = 0
         self.active_player = player_id
-
-    def move_pawns(self):
-        player = self.active_player
-        return self.player_grids[player].move_all_pawns()    
         
-    def update_non_active_player(self, losers: int):
+    def update_non_active_player(self, losers: int) -> None:
         inactive_player = 0
         if self.active_player == 0:
             inactive_player = 1
-        self.player_grids[inactive_player].multiply_all()
+        self.players[inactive_player].multiply_all(self.multiplier)
         if losers > 0:
-            self.player_grids[inactive_player].remove_losers(losers)
+            self.players[inactive_player].remove_losers(losers)
 
-    def take_turn(self):
-        winners_this_round = self.move_pawns()
-        self.winning_universes[self.active_player] += winners_this_round
-        self.update_non_active_player(winners_this_round)
+    def take_turn(self) -> None:
+        winning_pawns = self.players[self.active_player].move_all_pawns()
+        self.update_non_active_player(winning_pawns)
         self.next_player()
         
-class DiracGridManager:
+class DiracPlayer:
 
-    def __init__(self):
-        self.grid = DiracGrid(10,21)
-        self.next_grid = DiracGrid(10,21)
+    def __init__(self) -> None:
+        self.board_spaces = 10
+        self.target_score = 21
+        self.pawns = self.new_blank_grid()
+        self.wins = 0
 
-    def remove_losers(self, losers: int) -> None:
-        self.grid.remove_losers(losers)
+    def new_blank_grid(self) -> list:
+        grid = [[0] * (self.board_spaces+1) for _ in range(self.target_score + self.board_spaces)]
+        return grid
+
+    def set_start_position(self, starting_position: int) -> None:
+        self.pawns[0][starting_position] = 1
 
     def active_pawns(self) -> int:
-        return self.grid.active_pawns()
+        total_pawns = 0
+        for score in range(0,21):
+            total_pawns += sum(self.pawns[score])
+        return total_pawns
 
     def handle_winning_pawns(self) -> int:
         winners = 0
         for score in range(21,31):
             for place in range(1,11):
-                winners += self.grid.grid[score][place]
-                self.grid.grid[score][place] = 0
+                winners += self.pawns[score][place]
+                self.pawns[score][place] = 0
+        self.wins += winners
         return winners
-
-    def multiply_all(self):
-        self.grid.multiply_all(27)
     
-    def move_pawn_group(self, place, current_score):
+    def remove_losers(self, losers: int) -> None:
+        total_pawns = self.active_pawns()
+        remainers = total_pawns - losers
+        for i in range(len(self.pawns)):
+            for j in range(len(self.pawns[0])):
+                no_pawns = self.pawns[i][j]
+                no_pawns *= remainers
+                no_pawns /= total_pawns
+                self.pawns[i][j] = no_pawns
+
+    def multiply_all(self, multiplier) -> None:
+        for i in range(len(self.pawns)):
+            for j in range(len(self.pawns[0])):
+                self.pawns[i][j] *= multiplier
+    
+    def move_pawn_group(self, place, current_score, new_pawn_locations) -> None:
         distribution = [0,0,0,1,3,6,7,6,3,1] #Probability ditribution of die results
-        no_pawns = self.grid.grid[current_score][place]
+        no_pawns = self.pawns[current_score][place]
         for this_roll in range(3,10): #valid roles are 3,4,5,6,7,8,9
             new_place = place + this_roll
             if new_place > 10:
                 new_place -= 10
             new_score = new_place + current_score
-            self.next_grid.grid[new_score][new_place] += no_pawns * distribution[this_roll]
+            new_pawn_locations[new_score][new_place] += no_pawns * distribution[this_roll]
 
-    def move_pawns_with_score(self, score):
+    def move_pawns_with_score(self, score, new_pawn_locations) -> None:
         for place in range(1,11):
-            self.move_pawn_group(place, score)
+            self.move_pawn_group(place, score, new_pawn_locations)
 
     def move_all_pawns(self) -> int:
+        new_pawn_locations = self.new_blank_grid()
         for score in range(0,21): #Only move pawns that haven't won yet
-            self.move_pawns_with_score(score)
-        self.grid = self.next_grid
-        self.next_grid = DiracGrid(10,21)
+            self.move_pawns_with_score(score, new_pawn_locations)
+        self.pawns = new_pawn_locations
         return self.handle_winning_pawns()
-                
-class DiracGrid:
-
-    def __init__(self,board_spaces, target_score):
-        self.grid = [[0] * (board_spaces+1) for _ in range(target_score + board_spaces)]
-
-    def active_pawns(self) -> int:
-        total_pawns = 0
-        for score in range(0,21):
-            total_pawns += sum(self.grid[score])
-        return total_pawns
-
-    def remove_losers(self, losers: int) -> None:
-        total_pawns = self.active_pawns()
-        remainers = total_pawns - losers
-        for i in range(len(self.grid)):
-            for j in range(len(self.grid[0])):
-                pawns = self.grid[i][j]
-                pawns *= remainers
-                pawns /= total_pawns
-                self.grid[i][j] = pawns
-
-
-    def multiply_all(self, multiplier):
-        for i in range(len(self.grid)):
-            for j in range(len(self.grid[0])):
-                self.grid[i][j] *= multiplier

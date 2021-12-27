a7a9f5b7404dbba42b1cc3f1d113c8ec18add2aa
Neil <31366098+neilneil2000@users.noreply.github.com>
2021-12-27 16:44:47 +0000

    Day 23 Optimised Model

--- Day23/Amphipod.py
+++ Day23/Amphipod.py
@@ -8,69 +8,251 @@ class Rules:
     copper_only = 5
     dessert_only = 6
 
-
 class Amphipod:
     
-    Halls = { 'A': 3,
-    'B' : 5,
-    'C' : 7,
-    'D' : 9}
-
-    Costs = { 'A' :1,
-    'B' : 10,
-    'C' : 100,
-    'D' : 1000
-    }
+    COSTS = { 'A' :1,
+              'B' : 10,
+              'C' : 100,
+              'D' : 1000 }
 
     def __init__(self, colour, position):
         self.colour = colour
-        self.position = position
-        self.cost = Amphipod.Costs[colour]
+        self.positions = []
+        self.positions.append(position)
+        self.position = self.positions[-1]
+        self.cost = Amphipod.COSTS[colour]
+        self.done = False #True when in final position
+
+    def rewind(self):
+        """Step back (used when going back through recursive solution finder)"""
+        previous_position = self.positions.pop()
+        self.position = self.positions[-1]
+        self.done = False
+        return previous_position
+
+    def move(self, end: tuple) -> int:
+        """Move to new position and return cost of doing so"""
+        start_x, start_y = self.position
+        end_x, end_y = end
+
+        space_counter  = abs(start_x - end_x) # Horizontal move
+        space_counter += start_y - 1
+        space_counter += end_y - 1
+
+        self.positions.append(end)
+        if len(self.positions) > 3:
+            print(f'ERROR TOO MANY MOVES')
+        self.position = self.positions[-1]
+        return space_counter * self.cost
 
 class AmphipodGrid:
 
-    location_rules = {
-        (0, 0) : Rules.no_rules,
-        (1, 0) : Rules.no_rules,
-        (2, 0) : Rules.transit_only,
-        (2, 1) : Rules.amber_only,
-        (2, 2) : Rules.amber_only,
-        (2, 3) : Rules.amber_only,
-        (2, 4) : Rules.amber_only,
-        (3, 0) : Rules.no_rules,
-        (4, 0) : Rules.transit_only,
-        (4, 1) : Rules.bronze_only,
-        (4, 2) : Rules.bronze_only,
-        (4, 3) : Rules.bronze_only,
-        (4, 4) : Rules.bronze_only,
-        (5, 0) : Rules.no_rules,
-        (6, 0) : Rules.transit_only,
-        (6, 1) : Rules.copper_only,
-        (6, 2) : Rules.copper_only,
-        (6, 3) : Rules.copper_only,
-        (6, 4) : Rules.copper_only,
-        (7, 0) : Rules.no_rules,
-        (8, 0) : Rules.transit_only,
-        (8, 1) : Rules.dessert_only,
-        (8, 2) : Rules.dessert_only,
-        (8, 3) : Rules.dessert_only,
-        (8, 4) : Rules.dessert_only,
-        (9, 0) : Rules.no_rules,
-        (10,0) : Rules.no_rules
-    }
+    ROOMS = {   'A' : { (3,2), (3,3)},# (3,4), (3,5) },
+                'B' : { (5,2), (5,3)},# (5,4), (5,5) },
+                'C' : { (7,2), (7,3)},# (7,4), (7,5) },
+                'D' : { (9,2), (9,3)},# (9,4), (9,5) },
+                #'All': { (3,2), (3,3), (3,4), (3,5), (5,2), (5,3), (5,4), (5,5), (7,2), (7,3), (7,4), (7,5), (9,2), (9,3), (9,4), (9,5) }}
+                'All': { (3,2), (3,3), (5,2), (5,3), (7,2), (7,3), (9,2), (9,3) }}
+
+    HALL_STOP_POINTS = { (1,1), (2,1), (4,1), (6,1), (8,1), (10,1), (11,1) }
+        
 
     def __init__(self, filename):
+        self.room_status = { 'A' : False,
+                            'B' : False,
+                            'C' : False,
+                            'D' : False } #Is hall ready to accept incoming Amphipods? (i.e. is it empty or only contains correct Amphipods)
+        self.game_complete = False
+        self.winning_score  = 0
         warren_data = self.read_input_file(filename)
         self.amphipods = []
         self.location_occupancy = {}
         self.setup_warren(warren_data)
-        self.transit_only = self.get_transit_only_spaces()
-        self.get_legal_moves()
+        self.check_rooms()
+        self.is_complete()
 
-    def get_transit_only_spaces(self):
-        """Manual Hack for now - later check if has >2 neighbours"""
-        return {(3,1),(5,1),(7,1),(9,1)}
+    def force_game(self):
+        total_cost = 0
+        total_cost +=self.amphipods[2].move((4,1))
+        total_cost +=self.amphipods[1].move((7,2))
+        total_cost +=self.amphipods[5].move((6,1))
+        total_cost +=self.amphipods[2].move((5,3))
+        total_cost +=self.amphipods[0].move((5,2))
+        total_cost +=self.amphipods[3].move((8,1))
+        total_cost +=self.amphipods[7].move((10,1))
+        total_cost +=self.amphipods[3].move((9,3))
+        total_cost +=self.amphipods[5].move((9,2))
+        total_cost +=self.amphipods[7].move((3,2))
+        print(f'Total cost: {total_cost}')
+
+    def min_score_from_here(self):
+        """Returns a rough minimum score from here, always lower than reality"""
+        total = 0
+        for pod in self.amphipods:
+            x,y = pod.position
+            colour = pod.colour
+            cost = Amphipod.COSTS[colour]
+            if pod.position not in AmphipodGrid.ROOMS[colour]:
+                if colour == 'A':
+                    target = (3,2)
+                elif colour == 'B':
+                    target = (5,2)
+                elif colour == 'C':
+                    target = (7,2)
+                elif colour == 'D':
+                    target = (9,2)
+                target_x, target_y = target
+
+                total += abs(x - target_x) * cost # Horizontal move
+                total += (target_y - 1) * cost
+                total += (target_y - 1) * cost
+        return total
+
+
+    def compute_best_score(self):
+        moves = self.get_possible_moves()
+    
+        best_cost = 99999999999999
+        for move in moves:
+            print(f'*****************************************************')
+            print(f'   Attempting {move[0].colour} : {move[0].position}  -> {move[1]} as first move  ')
+            print(f'*****************************************************')
+            pod, location  = move
+            solution_cost = (self.move_manager(pod, location, 0, best_cost))
+            prev = pod.rewind()
+            if prev != location:
+                print(f"It's Gone Wrong!")
+            self.location_occupancy[pod.position] = True
+            self.location_occupancy[prev] = False
+            best_cost = min(best_cost, solution_cost)
+            print()
+            print(solution_cost)
+            print()
+                
+        self.winning_score = best_cost
+                
+        print(f'Winning score is: {self.winning_score}')
+
+    def is_complete(self) -> bool:
+        """Checks Amphipod locations and updates self.game_complete"""
+        for pod in self.amphipods:
+            if pod.position not in AmphipodGrid.ROOMS[pod.colour]:
+                return False
+        #self.game_complete = True
+        return True
+    
+    def check_rooms(self) -> None:
+        """Update hall status to see if they are ready to accept incoming Amphipods"""
+        Readiness = { 'A' : True, 'B' : True, 'C' : True, 'D' : True }
+        for pod in self.amphipods:
+            position = pod.position
+            if position not in AmphipodGrid.ROOMS[pod.colour]:
+                if position in AmphipodGrid.ROOMS['A']:
+                    Readiness.update({ 'A': False })
+                elif position in AmphipodGrid.ROOMS['B']: 
+                    Readiness.update({ 'B': False })
+                elif position in AmphipodGrid.ROOMS['C']: 
+                    Readiness.update({ 'C': False })
+                elif position in AmphipodGrid.ROOMS['D']:
+                    Readiness.update({ 'D': False })
+        for hall in Readiness:
+            self.room_status[hall] = Readiness[hall] 
+
+    def move(self, amphipod: Amphipod, location: tuple):
+        cost = amphipod.move(location)
+        if amphipod.position in AmphipodGrid.ROOMS[amphipod.colour] and \
+            self.room_status[amphipod.colour] == True:
+            amphipod.done = True
+        #Set done if appropriate
+        return cost
+
+    def move_manager(self, amphipod: Amphipod, location: tuple, total_cost: int, best_cost: int) -> int:
+        """Move Amphipod to new location and add cost to total"""
+        previous_location = amphipod.position
+        total_cost += self.move(amphipod, location) #Do move and add cost to total
+        self.location_occupancy[location] = True
+        self.location_occupancy[previous_location] = False
+        if total_cost >= best_cost:
+            return total_cost #Don't bother going further than the best solution so far
+        if self.min_score_from_here() + total_cost > best_cost:
+            return best_cost #No routes from here can beat the best
+        if self.is_complete() == True:
+            print(f'{total_cost}: New Best Solution Found')
+            return total_cost # Return total cost for solution
         
+        moves = self.get_possible_moves()
+        self.check_rooms()
+        for move in moves:
+            pod, move_location = move
+            #Only attempt if not in home space
+            if pod.done == False:
+                solution_cost = self.move_manager(pod, move_location, total_cost, best_cost)
+                prev = pod.rewind()
+                if prev != move_location:
+                    print(f"It's Gone Wrong!")
+                self.location_occupancy[pod.position] = True
+                self.location_occupancy[prev] = False
+                best_cost = min(best_cost, solution_cost)
+        return best_cost
+
+    def get_possible_moves(self) -> list:
+        """Returns list of all possible moves. Each list entry is (Amphipod, destination:tuple)"""
+        #For each Amphipod work out next legal moves
+        moves = []
+        for amphipod in self.amphipods:
+            if amphipod.done == False:
+                end_points = self.get_legal_endpoints(amphipod)
+                reachables = self.get_reachable_points(amphipod.position, amphipod.position, set())
+                end_points.intersection_update(reachables)
+                pass
+                for location in end_points:
+                    moves.append((amphipod, location))
+
+        return moves
+
+    def get_reachable_points(self, position: tuple, from_position: tuple, reachable_so_far: set) -> set:
+        """Get squares that are reachable from *position*"""
+        if position not in self.location_occupancy:
+            return reachable_so_far # We've hit a wall
+        if self.location_occupancy[position] == True and position != from_position:
+            return reachable_so_far # We've hit another player
+        else:
+            x,y = position
+            neighbours = {(x+1, y),(x-1, y),(x, y+1),(x, y-1)}
+            neighbours.discard(from_position)
+            for neighbour in neighbours:
+                neighbour_points = self.get_reachable_points(neighbour, position, reachable_so_far)
+                reachable_so_far = reachable_so_far.union(neighbour_points)
+        if position != from_position:
+            reachable_so_far.add(position) #This is a legal position
+        return reachable_so_far
+
+    def get_legal_endpoints(self, amphipod) -> set:
+        self.check_rooms()
+        position = amphipod.position
+        end_points = set()
+
+        if self.room_status[amphipod.colour]:
+            if position in AmphipodGrid.ROOMS[amphipod.colour]:
+                return end_points #I'm already in a home spot so no moves sensible from here - return empty set
+            else: #Add lowest free spot in room
+                room_spaces = AmphipodGrid.ROOMS[amphipod.colour].copy()
+                free_room_spaces = set()
+                for space in room_spaces:
+                    if self.location_occupancy[space] == False:
+                        free_room_spaces.add(space)
+                if len(free_room_spaces) > 1:
+                    y_max = 0
+                    for space in free_room_spaces:
+                        x,y = space
+                        y_max = max(y, y_max)
+                    free_room_spaces = {(x,y_max)}
+                end_points = end_points.union(free_room_spaces)
+
+        if position in AmphipodGrid.ROOMS['All']:
+            end_points = end_points.union(AmphipodGrid.HALL_STOP_POINTS) #If I'm in a room I can stop at hall stop points      
+
+        return end_points
 
     def setup_warren(self,warren_data):
         occupancy = {}
@@ -86,47 +268,6 @@ class AmphipodGrid:
 
         self.location_occupancy = occupancy
 
-    def get_empty_squares(self) -> set:
-        empty_squares = set()
-        for location, status in self.location_occupancy.items():
-            if status == False:
-                empty_squares.add(location)
-        return empty_squares
-
-    def get_legal_moves(self):
-        end_points = self.get_empty_squares()
-        for square in self.transit_only:
-            end_points.discard(square)
-        
-        #Get Customised List per amphipod
-        for amphipod in self.amphipods:
-            my_end_points = end_points.copy()
-            for square in end_points:
-                x,y = square
-                if y > 1:   #Remove rooms for other colours
-                    if amphipod.colour == 'A':
-                        if x != 3:
-                            my_end_points.discard(square)
-                    elif amphipod.colour == 'B':
-                        if x != 5:
-                            my_end_points.discard(square)
-                    elif amphipod.colour == 'C':
-                        if x != 7:
-                            my_end_points.discard(square)
-                    elif amphipod.colour == 'D':
-                        if x != 9:
-                            my_end_points.discard(square)
-            #Remove own hall if contains a non-correct colour
-            #remove all but bottom of call
-                
-
-            print(amphipod.colour)
-
-
-
-
-       
-        
     def read_input_file(self, filename):
         raw_data =[]
         with open(filename, 'r') as f:
--- Day23/Day23.py
+++ Day23/Day23.py
@@ -1,7 +1,9 @@
-from Amphipod import Amphipod,AmphipodGrid
+from Amphipod import AmphipodGrid
 
 def main():
-    large_warren = AmphipodGrid("Day23\DayTwentyThreeTestInput")
+    large_warren = AmphipodGrid("Day23\DayTwentyThreePart2Input")
+    #large_warren.force_game()
+    large_warren.compute_best_score()
     print(f'END')
 
 

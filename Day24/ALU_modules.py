32d2b91141c3f034173a8a3dc747ea82a5304b0b
Neil <31366098+neilneil2000@users.noreply.github.com>
2021-12-30 18:17:38 +0000

    Day 24 Part 1 Complete

--- Day24/ALU_modules.py
+++ Day24/ALU_modules.py
@@ -15,6 +15,24 @@ class ModelDigit:
         for input in inputs:
             self.valid_inputs.add(input)
 
+    def set_outputs(self, outputs):
+        self.target_outputs = set()
+        for output in outputs:
+            self.target_outputs.add(output)
+
+    def attempt(self, model_digit, z_input):
+        z = z_input
+        if self.divide_by_26==True:
+                z = int(z_input/26)
+        if z_input % 26 + self.add_to_modulus != model_digit:
+            z *= 26
+            z += model_digit + self.add_to_output
+        return z
+
+        
+
+
+
     def method_one(self):
         """output = trunc(input/divisor) IF mod in {1-9}"""
         if self.add_to_modulus > max(ModelDigit.VALID_DIGITS):
@@ -36,6 +54,84 @@ class ModelDigit:
             for model_digit in range(9,0,-1):
                 if (z % 26) + self.add_to_modulus != model_digit: #This would mean it would take the other path
                     self.valid_outputs.add(z_out+model_digit)
-                
+
+    def calculate_valid_inputs(self):
+        """There are two possible scenarios in two types"""
+        if self.divide_by_26 == False:
+            possible_inputs = self.target_outputs.copy() #Scenario 1: z_out = z_in
+            for output in self.target_outputs:
+                for digit_value in range(9,0,-1):
+                    interim_value = output - digit_value - self.add_to_output
+                    if interim_value >= 0:
+                        if interim_value % 26 == 0:
+                            input = int(interim_value/26)
+                            possible_inputs.add(input) #Scenario 2: z_out = 26z_in + digit_value + output_add
+
+        else: #self.divide_by_26 == True:
+            possible_inputs = set()
+            for output in self.target_outputs:
+                output *= 26
+                for i in range(0,26):
+                    possible_inputs.add(output + i) #Scenario 1: z_out = trunc(z_in/26)
+            for output in self.target_outputs:
+                for digit_value in range(9,0,-1):
+                    interim_value = output - digit_value - self.add_to_output
+                    if interim_value >= 0:
+                        if interim_value % 26 == 0:
+                            input = int(interim_value/26)
+                            for i in range(0,26):
+                                possible_inputs.add(input + i) #Scenario 2: z_out = 26trunc(z_in/26) + digit_value + output_add
+                  
 
         
+        #Validate the possible values
+        for input in possible_inputs:
+            for digit_value in range(9,0,-1):
+                if self.attempt(digit_value, input) in self.target_outputs:
+                    self.valid_inputs.add(input)
+
+
+
+
+
+
+
+    def reverse_method_one(self):
+        """output = trunc(input/divisor) IF mod in {1-9}"""
+        if self.divide_by_26 == False:
+            for output in self.target_outputs:
+                if output % 26 + self.add_to_modulus in ModelDigit.VALID_DIGITS:
+                    self.valid_inputs.add(output)
+        else:
+            for output in self.target_outputs:
+                input = output * 26
+                input_range = set()
+                for i in range(0,26):
+                    input_range.add(input + i)
+                for input in input_range:
+                    if input % 26 + self.add_to_modulus in ModelDigit.VALID_DIGITS:
+                        self.valid_inputs.add(input)
+                
+
+    def reverse_method_two(self):
+        """output = 26trunc(input/divisor) + model_digit + add2 IF mod NOT in {1-9}"""
+        if self.divide_by_26 == False:
+            for output in self.target_outputs:
+                for digit_value in range(9,0,-1):
+                    possible_input = output - digit_value - self.add_to_output
+                    if possible_input % 26 + self.add_to_modulus != digit_value:
+                        self.valid_inputs.add(possible_input)
+        else:
+            for output in self.target_outputs:
+                for digit_value in range(9,0,-1):
+                    possible_input = output - digit_value - self.add_to_output
+                    if possible_input % 26 == 0:
+                        possible_input = int(possible_input/26)
+                        input_range = set()
+                        for i in range(0,26):
+                            input_range.add(possible_input + i)
+                        for input in input_range:
+                            if input % 26 + self.add_to_modulus in ModelDigit.VALID_DIGITS:
+                                self.valid_inputs.add(input)
+                            elif output - self.add_to_output in ModelDigit.VALID_DIGITS:
+                                self.valid_inputs.add(input)
--- Day24/Day24_2.py
+++ Day24/Day24_2.py
@@ -19,6 +19,79 @@ def main():
     digits.append(ModelDigit(True, -11, 5))
 
     next_inputs = {0}
+    target_output = {0}
+
+    for i in range(13,6,-1):
+        digits[i].set_outputs(target_output)
+        start = time.perf_counter()
+        digits[i].calculate_valid_inputs()
+        end = time.perf_counter()
+        target_output = digits[i].valid_inputs
+        print(f'{len(target_output)} Valid Inputs found in { int(end-start)} seconds ')
+    pass
+
+    for a in range(9,0,-1):
+        a_out = digits[0].attempt(a,0)
+        for b in range(9,0,-1):
+            b_out = digits[1].attempt(b,a_out)
+            for c in range(9,0,-1):
+                c_out = digits[2].attempt(c, b_out)
+                for d in range(9,0,-1):
+                    d_out = digits[3].attempt(d, c_out)
+                    for e in range(9,0,-1):
+                        e_out = digits[4].attempt(e, d_out)
+                        for f in range(9,0,-1):
+                            f_out = digits[5].attempt(f, e_out)
+                            if True: #if_out in digits[6].valid_inputs:
+                                for g in range(9,0,-1):
+                                    g_out = digits[6].attempt(g, f_out)
+                                    if True: #g_out in digits[7].valid_inputs:
+                                        for h in range(9,0,-1):
+                                            h_out = digits[7].attempt(h, g_out)
+                                            if h_out in digits[8].valid_inputs:
+                                                for i in range(9,0,-1):
+                                                    i_out = digits[8].attempt(i, h_out)
+                                                    if i_out in digits[9].valid_inputs:
+                                                        for j in range(9,0,-1):
+                                                            j_out = digits[9].attempt(j, i_out)
+                                                            if j_out in digits[10].valid_inputs:
+                                                                for k in range(9,0,-1):
+                                                                    k_out = digits[10].attempt(k, j_out)
+                                                                    if k_out in digits[11].valid_inputs:
+                                                                        for l in range(9,0,-1):
+                                                                            l_out = digits[11].attempt(l, k_out)
+                                                                            if l_out in digits[12].valid_inputs:
+                                                                                for m in range(9,0,-1):
+                                                                                    m_out = digits[12].attempt(m, l_out)
+                                                                                    if m_out in digits[13].valid_inputs:
+                                                                                        for n in range(9,0,-1):
+                                                                                            n_out = digits[13].attempt(n, m_out)
+                                                                                            if n_out == 0:
+                                                                                                print(f'SOLUTION FOUND! {a}{b}{c}{d}{e}{f}{g}{h}{i}{j}{k}{l}{m}{n}')
+                                                                                                print(f'z_0 = 0')
+                                                                                                print(f'z_1 = {a_out}')
+                                                                                                print(f'z_2 = {b_out}')
+                                                                                                print(f'z_3 = {c_out}')
+                                                                                                print(f'z_4 = {d_out}')
+                                                                                                print(f'z_5 = {e_out}')
+                                                                                                print(f'z_6 = {f_out}')
+                                                                                                print(f'z_7 = {g_out}')
+                                                                                                print(f'z_8 = {h_out}')
+                                                                                                print(f'z_9 = {i_out}')
+                                                                                                print(f'z_10 = {j_out}')
+                                                                                                print(f'z_11 = {k_out}')
+                                                                                                print(f'z_12 = {l_out}')
+                                                                                                print(f'z_13 = {m_out}')
+                                                                                                print(f'z_14 = {n_out}')
+
+
+
+
+
+
+
+
+
 
     for digit in digits:
         digit.set_inputs(next_inputs)

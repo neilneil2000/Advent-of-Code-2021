824be0333daf5fbe5250b18371dfde77b6bb8114
Neil <31366098+neilneil2000@users.noreply.github.com>
2022-01-03 18:22:26 +0000

    Day 22 with Manual test data

--- Day22/day_22_second_attempt.py
+++ Day22/day_22_second_attempt.py
@@ -27,6 +27,19 @@ class Reactor:
 
     def count_lit_cubes(self) -> int:
         """Counts number of lit cubes in self.on_blocks"""
+        lit_cubes = 0
+        for block in self.on_blocks:
+            lit_cubes += self.count_cubes_in_block(block)
+
+        return lit_cubes
+
+    def count_cubes_in_block(self, block: Block) -> int:
+        """Counts number of cubes in block"""
+        x = block.x_dimension[1] - block.x_dimension[0] + 1
+        y = block.y_dimension[1] - block.y_dimension[0] + 1
+        z = block.z_dimension[1] - block.z_dimension[0] + 1
+
+        return x * y * z
 
     def execute_boot_sequence(self, instructions: List[Instruction]) -> None:
         """Run through all boot sequence instructions"""
@@ -44,28 +57,32 @@ class Reactor:
             print("Invalid Block State")
             print(block)
 
-    def process_on_block(self, unprocessed_blocks: List[Block]) -> None:
+    def process_on_block(self, on_block: Block) -> None:
         """Update Reactor to turn ON this block"""
+        unprocessed_blocks = [on_block]
         for existing_block in self.on_blocks:
-            result_blocks = []
-            for block in unprocessed_blocks:
-                result_blocks.append(self.underlap(block, existing_block))
-            if result_blocks == []:
+            splinters = []
+            for new_block in unprocessed_blocks:
+                overlap = self.overlap(existing_block, new_block)
+                if overlap is None:
+                    splinters.append(new_block)
+                else:
+                    splinters.extend(self.subtract_blocks(new_block, overlap))
+            if splinters == []:
                 return  # Block is entirely covered by existing blocks
-            unprocessed_blocks = result_blocks
+            unprocessed_blocks = splinters
         if unprocessed_blocks != []:
-            for block in unprocessed_blocks:
-                self.on_blocks.append(block)
+            for new_block in unprocessed_blocks:
+                self.on_blocks.append(new_block)
 
-        """WRITING OUT LONG HAND TO HELP MY BRAIN"""
-        # 1. Compare new block to first existing block. Possible Outcomes:
-        #   - No overlap: return entire new_block
-        #   - Total overlap: return [] / None
-        #   - Partial overlap: retun underlapping areas as a list of blocks
-        # 2. If Total overlap - return -> We are Done
-        #   If No overlap - try next existing block
-        #   If Partial overlap: try next existing block against each of the remaining on blocks
-        # 3. If no more existing blocks to try: Add list of remaining blocks to on_list
+    def process_off_block(self, off_block: Block) -> None:
+        """Update Reactor to turn OFF this block"""
+        current_blocks = self.on_blocks.copy()
+        for on_block in current_blocks:
+            overlap = self.overlap(on_block, off_block)
+            if overlap is not None:
+                self.on_blocks.extend(self.subtract_blocks(on_block, overlap))
+                self.on_blocks.remove(on_block)
 
     def chunk_1d(
         self, small: Tuple[int, int], large: Tuple[int, int]
@@ -138,68 +155,60 @@ class Reactor:
             return None
         return overlap_low, overlap_high
 
-    def underlap_1d(
-        self, new_line: Tuple[int, int], existing_line: Tuple[int, int]
-    ) -> List[Tuple[int, int]]:
-        """
-        Calculates underlap between two 1 dimensional lines
-        Returns List of sections
-
-        """
-        new_low, new_high = new_line
-        existing_low, existing_high = existing_line
-        underlaps = []
-
-        if new_low > existing_high:
-            # +--existing--+
-            #                   +--new--+
-            return [new_line]
-        if new_high < existing_low:
-            #                   +--existing--+
-            # +--new--+
-            return [new_line]
-        if new_low >= existing_low and new_high <= existing_high:
-            # +--existing--+
-            #    +--new--+
-            return []
-        if new_low < existing_low:
-            #      +--existing-------------
-            # +--new-----------------------
-            underlaps.append((new_low, existing_low - 1))
-        if new_high > existing_high:
-            # ---existing--+
-            # ------------------------new--+
-            underlaps.append(existing_high + 1, new_high)
-        return underlaps
-
-    def underlap(self, new_block: Block, existing_block: Block) -> List[Block]:
-        """Calculates area of overlap or underlap and returns underlap"""
-        x_underlaps = self.underlap_1d(
-            new_block.x_dimension, existing_block.x_dimension
-        )
-        y_underlaps = self.underlap_1d(
-            new_block.y_dimension, existing_block.y_dimension
-        )
-        z_underlaps = self.underlap_1d(
-            new_block.z_dimension, existing_block.z_dimension
-        )
-        # How do I compile these, especially if one has zero underlap
-        underlaps = []
-
-    def process_off_block(self, block: Block):
-        """Update Reactor to turn OFF this block"""
-
 
 def main():
-    big_block = Block((0, 100), (0, 100), (0, 100))
-    small_block = Block((0, 90), (0, 90), (0, 90))
     my_reactor = Reactor()
-    overlapping_block = my_reactor.overlap(big_block, small_block)
-    if overlapping_block == small_block:
-        print("Overlap Correct")
-    remaining_blocks = my_reactor.subtract_blocks(big_block, small_block)
-    for block in remaining_blocks:
+    block_a = Block((0, 9), (0, 9), (0, 9))
+    block_b = Block((6, 15), (6, 15), (6, 15))
+
+    my_reactor.process_on_block([block_a])
+    my_reactor.process_on_block([block_b])
+    my_reactor.process_off_block(block_a)
+    my_reactor.process_off_block(block_b)
+
+    print(f"Block A: {my_reactor.count_cubes_in_block(block_a)} Cubes\n{block_a}\n")
+    print(f"Block B: {my_reactor.count_cubes_in_block(block_b)} Cubes\n{block_b}\n")
+
+    overlapping_block = my_reactor.overlap(block_a, block_b)
+    print(
+        f"Overlapping Block: {my_reactor.count_cubes_in_block(overlapping_block)} Cubes\n{overlapping_block}\n"
+    )
+    new_blocks = my_reactor.subtract_blocks(block_b, overlapping_block)
+
+    overlap_cubes = 0
+    for block in new_blocks:
+        overlap_cubes += my_reactor.count_cubes_in_block(block)
+    print(f"Additional Blocks: {overlap_cubes} Cubes")
+    for block in new_blocks:
         print(block)
+
+
+def process_input_file(input: list) -> list:
+    output = []
+    for line in input:
+        on_off, co_ords = line.split(" ")
+        co_ords = co_ords.split(",")
+        x, y, z = co_ords
+        x = x.split("=")[1].split("..")
+        x[0] = int(x[0])
+        x[1] = int(x[1])
+        y = y.split("=")[1].split("..")
+        y[0] = int(y[0])
+        y[1] = int(y[1])
+        z = z.split("=")[1].split("..")
+        z[0] = int(z[0])
+        z[1] = int(z[1])
+        cuboid = (on_off, (x, y, z))
+        output.append(cuboid)
+    return output
+
+
+def read_input_file(filename) -> list:
+    input = []
+    with open(filename, "r") as f:
+        for line in f:
+            input.append(line.strip())
+    return input
 
 
 if __name__ == "__main__":
